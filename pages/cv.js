76177d8f150c38c125f4653c1a3306fc0bf02702
liam-nguyen <nguyen.dch@gmail.com>
2020-06-18 16:30:40 -0700

    Add CV page

--- pages/cv.js
+++ pages/cv.js
@@ -1,9 +1,26 @@
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
+import { Row, Col } from 'reactstrap';
 
 const CV = () => (
   <BaseLayout>
-    <BasePage title="I'm CV page"></BasePage>
+    <BasePage title="I'm CV page" customClassName='cv-page'>
+      <Row>
+        <Col md={{ size: 8, offset: 2 }}>
+          <div className='cv-title'>
+            <a
+              className='btn btn-success'
+              download='resume.pdf'
+              href='files/resume_v7.pdf'>
+              Download
+            </a>
+          </div>
+          <iframe
+            src='files/resume_v7.pdf'
+            style={{ width: '100%', height: '800px' }}></iframe>
+        </Col>
+      </Row>
+    </BasePage>
   </BaseLayout>
 );
 
